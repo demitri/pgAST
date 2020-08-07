9c961767c248b3ff5fe8f29f9e3d3023a3145b43
Demitri Muna <github@demitri.com>
2020-08-07 19:03:12 -0400

    Updating Xcode project used for testing.

---
+++ examples/ast_sandboxes/read_fits_header/read_fits_header.h
@@ -0,0 +1,14 @@
+//
+//  read_fits_header.h
+//  ast_sandboxes
+//
+//  Created by Demitri Muna on 5/12/20.
+//  Copyright © 2020 Demitri Muna. All rights reserved.
+//
+
+#ifndef read_fits_header_h
+#define read_fits_header_h
+
+#include <stdio.h>
+
+#endif /* read_fits_header_h */
